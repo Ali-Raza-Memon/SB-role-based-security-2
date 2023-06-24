8148cd278b79a91cd28e1f245cc1652098868a4e
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-24 13:13:24 +0500

    This will provide you role of user

--- src/main/java/com/UserManagementApp/config/CustomSuccessHandler.java
+++ src/main/java/com/UserManagementApp/config/CustomSuccessHandler.java
@@ -1,6 +1,7 @@
 package com.UserManagementApp.config;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 
@@ -8,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 public class CustomSuccessHandler implements AuthenticationSuccessHandler {
 
@@ -15,5 +17,12 @@ public class CustomSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
+//        will provide you roll
+//        authentication.getAuthorities()
+
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
     }
+
+
 }
